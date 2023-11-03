c14a4cafc96d0f137284c8fe2b08b1800bba2f35
brewdzyz <dad@brewd.xyz>
2023-11-03 07:10:30 -0400

    Update test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,21 +1,22 @@
 import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from server import app, get_openai_stream_data, RequestBody
+from server import app, RequestBody
 from unittest.mock import AsyncMock, patch
 
 client = TestClient(app)
 
-@pytest.mark.asyncio
-@patch("server.get_openai_stream_data", new_callable=AsyncMock)
-async def test_generate_stream(mock_get_openai_stream_data):
-    mock_get_openai_stream_data.return_value = iter([{
+async def mock_openai_stream_data(request):
+    yield {
         'choices': [{
             'delta': {'content': 'Test response'},
             'finish_reason': None
         }]
-    }])
+    }
 
+@pytest.mark.asyncio
+@patch("server.get_openai_stream_data", new=mock_openai_stream_data)
+async def test_generate_stream():
     request_body = {
         "inputs": "What is AI?",
         "parameters": {
