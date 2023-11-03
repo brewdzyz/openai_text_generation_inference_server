555f60fdf993c67526b6c528c3fc2e11929966ca
brewdzyz <dad@brewd.xyz>
2023-11-03 07:17:07 -0400

    Update test_server.py

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,17 +6,18 @@ from unittest.mock import AsyncMock, patch
 
 client = TestClient(app)
 
-async def mock_openai_stream_data(request):
-    yield {
-        'choices': [{
-            'delta': {'content': 'Test response'},
-            'finish_reason': None
-        }]
-    }
+class MockStream:
+    async def __aiter__(self):
+        yield {
+            'choices': [{
+                'delta': {'content': 'Test response'},
+                'finish_reason': None
+            }]
+        }
 
 @pytest.mark.asyncio
-@patch("server.get_openai_stream_data", new=mock_openai_stream_data)
-async def test_generate_stream():
+@patch("server.get_openai_stream_data", return_value=MockStream())
+async def test_generate_stream(mock_get_openai_stream_data):
     request_body = {
         "inputs": "What is AI?",
         "parameters": {
